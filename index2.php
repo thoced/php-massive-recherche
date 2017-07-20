e535572726d6eaf06eb1d66966892cced52d5197
Thonon <Thonon@Thonon-PC.lan>
2017-07-21 01:15:27 +0200

    - modification

--- index2.php
+++ index2.php
@@ -19,51 +19,19 @@ $obj = new ImportModel("demo.xls");
 
 $listSheet = $obj->getSheetNames();
 
-$lab = $obj->getLabelNamesBySheet("toto");
+$obj->setSelectedActiveSheet("toto");
 
-foreach($lab as $l)
+$lab = $obj->getLabelNamesBySheetActive();
+
+if($lab != null)
 {
-    echo "label : " . $l . "<br>";
+    foreach($lab as $l)
+    {
+        echo "label : " . $l . "<br>";
+    }
 }
 
 
-
-
-// chargement d'un fichier excel
-$objPHPExcel = PHPExcel_IOFactory::load("demo.xls");
-
-// obtention du nombre de sheet
-$nbSheet = $objPHPExcel->getSheetCount();
-
-// affichage des noms des sheet
-
-foreach($listSheet as $nameSheet)
-{
-  echo $nameSheet . "<br>";
-}
-
-// lecture du premier sheet
-$sheet = $objPHPExcel->getSheetByName('toto');
-
-
-
-echo '<table border="1">';
- 
-// On boucle sur les lignes
-foreach($sheet->getRowIterator() as $row ){
- 
-   echo '<tr>';
- 
-   // On boucle sur les cellules de la ligne
-   foreach ($row->getCellIterator() as $cell) {
-      echo '<td>';
-      print_r($cell->getValue());
-      echo '</td>';
-   }
- 
-   echo '</tr>';
-}
-echo '</table>';
 ?>
     </body>
 </html>
