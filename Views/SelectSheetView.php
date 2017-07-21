becadc9fb7b8680e138ac40ba03c591df16749fa
Thonon <Thonon@Thonon-PC.lan>
2017-07-21 16:27:33 +0200

    - ajout du size

--- Views/SelectSheetView.php
+++ Views/SelectSheetView.php
@@ -2,7 +2,7 @@
 Veuillez sélectionner l'onglet sur lesquel les recherches doivent être réalisées<br><br>
 Onglets : 
 <form action="../Controllers/SelectColumnController.php" method="POST">
-<select name="sheet">
+<select size="10" name="sheet">
 <?php
     foreach($listSheets as $sheet)
      echo "<option value=" . $sheet . ">" . $sheet . "</option>";
