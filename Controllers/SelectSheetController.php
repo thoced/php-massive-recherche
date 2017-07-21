f32268b5521d6d31718a6b599a4ecaed31135ccc
Thonon <Thonon@Thonon-PC.lan>
2017-07-21 15:15:14 +0200

    - Utilisation de la session et sérialisation de l'ojbet ImportModel pour pouvoir utiliser l'objet dans l'ensemble de la session

--- Controllers/SelectSheetController.php
+++ Controllers/SelectSheetController.php
@@ -5,15 +5,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+
 require_once  '../../Classes/PHPExcel.php';
 require_once  '../Models/ModelHelper.php';
 require_once  '../Models/ModelDbKeyWordHelper.php';
 
+session_start();
 // reception du fichier Excel
 $modelImport = new ImportModel("../demo.xls");
 
 // réception de la liste des sheets
 $listSheets = $modelImport->getSheetNames();
+
+// serialisatin et passage de l'objet dans la session
+$m = serialize($modelImport);
+$_SESSION['ImportModel'] = $m;
 
 // appel à la vue
 require '../Views/SelectSheetView.php';
