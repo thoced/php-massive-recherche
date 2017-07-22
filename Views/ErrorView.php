b27fcc82a5fee60eb770b8ca031de786300b3110
Thonon <Thonon@Thonon-PC.lan>
2017-07-22 16:25:04 +0200

    - Modification couleur

--- Views/ErrorView.php
+++ Views/ErrorView.php
@@ -1,11 +1,8 @@
 
+<div style="border-width: 4px;border-color:  #e74c3c;border-radius: 8px;border-style: solid; background-color:#f2d7d5;">
 <h1> Erreur </h1>
-<?php
-    echo $errno . "<br>";
-?>
+<?php echo $errno . "<br>";?>
 <br>
-
-<a href="../Index.php"">Retour à l'application</a>
-
-
-
+ </div>
+<br>
+<a href="../Index.php">Retour à l'application</a>
