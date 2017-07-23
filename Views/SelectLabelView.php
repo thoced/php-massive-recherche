b8086e05cb7471fdaaa0cb1422c1e9e896aade8c
Thonon <Thonon@Thonon-PC.lan>
2017-07-23 10:41:34 +0200

    - Amélioration de la vue dans la sélection du Label - ajout de balise DIV

--- Views/SelectLabelView.php
+++ Views/SelectLabelView.php
@@ -1,16 +1,25 @@
 
 Veuillez sélectionner le label sur lesquel les recherches doivent être réalisées<br><br>
-Onglets : 
 <form action="../Controllers/ComputeSearchController.php" method="POST">
-<select size="10" name="labelKey">
-<?php
-    while($label = current($listLabels))
-    {
-     echo "<option value=" . key($listLabels) . ">" . $label . "</option>";
-     next($listLabels);
-    }
-?>
-</select>
-    <br>    
-    <input type="submit" value="Suivant">
+<div style="border-width: 4px;border-color:#5499c7;border-radius: 8px;border-style: solid;background-color:#eaf2f8;">
+     <table width="100%">
+         <tr>
+             <td width="15%">Sélectionner un label: </td> 
+             <td widht="85%">
+                 <select size="10" name="labelKey">
+                     <?php
+                        while($label = current($listLabels))
+                        {
+                         echo "<option value=" . key($listLabels) . ">" . $label . "</option>";
+                         next($listLabels);
+                        }
+                     ?>
+                 </select>
+
+             </td>
+         </tr>
+</table>
+</div>    
+<br>    
+<input type="submit" value="Suivant">
 </form>
