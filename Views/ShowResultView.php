a7d3536036bb0a781a9984af60e693e41e8f361a
thonon <thonon@DESKTOP-LN0K664.lan>
2017-08-16 19:16:53 +0200

    ajout d'un tag CSS

--- Views/ShowResultView.php
+++ Views/ShowResultView.php
@@ -1,6 +1,6 @@
 <h1 style="text-align: center;">Résultat des recherches</h1>
 
-<div style="border-width: 4px;border-color:#5499c7;border-radius: 8px;border-style: solid; background-color:#eaf2f8;">
+<div style="border-width: 4px;border-color:#5499c7;border-radius: 8px;border-style: solid; background-color:#eaf2f8; overflow:auto;">
 <table width="80%">
     <tr>
         <td><b>Indice</b></td><td><b>Contenu</b></td><td><b>Mot clé utilisé</b></td>
